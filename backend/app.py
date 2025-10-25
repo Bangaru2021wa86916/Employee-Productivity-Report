43039e550b7cd87a9032bd80d96043412f7af5a3
Bangaru2021wa86916 <2021wa86916@wilp.bits-pilani.ac.in>
2025-10-25 17:45:58 +0000

    Updated app.py with new logic

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,5 @@
-from flask import Flask, jsonify, request, make_response
-from flask_jwt_extended import (
-    JWTManager, create_access_token, jwt_required,
-    get_jwt_identity, get_jwt
-)
+from flask import Flask, jsonify, request, make_response        
+from flask_jwt_extended import (JWTManager, create_access_token, jwt_required,get_jwt_identity, get_jwt)
 from flask_cors import CORS
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
