e5296a8406134abb058d8b9da96bd7ab216406f0
Bangaru2021wa86916 <2021wa86916@wilp.bits-pilani.ac.in>
2025-11-01 12:17:51 +0000

    Updated app.py with new logic

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+CORS(app)
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import mysql.connector
 import datetime
